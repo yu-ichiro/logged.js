feec624594bb2f8e90a209a8809fc79266d44d61
Yuichiro Smith <contact@yu-smith.com>
2021-02-11 19:09:48 +0900

    change interface

--- src/index.ts
+++ src/index.ts
@@ -19,69 +19,86 @@ const DEFAULT_LEVELS: Levels = {
 }
 
 export interface Loggable {
-  log(log: Log, level: number): void
-  trace(log: Log): void
-  debug(log: Log): void
-  info(log: Log): void
-  warn(log: Log): void
-  error(log: Log): void
-  fatal(log: Log): void
+  trace(...args: unknown[]): void
+
+  debug(...args: unknown[]): void
+
+  info(...args: unknown[]): void
+
+  warn(...args: unknown[]): void
+
+  error(...args: unknown[]): void
+
+  fatal(...args: unknown[]): void
 }
 
-export interface Log {
+export interface LogRecord {
+  level: string
   name: string
   createdAt: Date
   message: string
 }
 
 export interface Formatter {
-  format(log: Log): string
+  format(log: LogRecord): string
 }
 
 export interface Handler {
   level: number
 
-  log(log: Log): void
+  handle(log: LogRecord): void
+}
+
+export interface Builder {
+  build(...args: unknown[]): LogRecord
+}
+
+export const defaultBuilder: Builder = {
+  build(this: Logger, ...args: unknown[]): LogRecord {
+    if (args.length < 1) throw Error("at least one argument is required")
+    const firstArg = args.shift()
+    let partialRecord = {
+      message: undefined as string | undefined,
+      args: args
+    }
+    if (typeof firstArg === "object") {
+      partialRecord = {...partialRecord, ...firstArg};
+    } else if (typeof firstArg === "string") {
+      partialRecord.message = firstArg
+    } else {
+      partialRecord.message = String(firstArg)
+    }
+    if (!partialRecord.message) throw Error("message not specified")
+
+    return {
+      level: "",
+      name: this.name,
+      createdAt: new Date(),
+      ...partialRecord,
+    } as LogRecord
+  }
 }
 
 class Logger implements Loggable {
-  private name: string
+  private _name: string
   parent?: Logger
   propagate: boolean
   defaultLevel: number
   children: Record<string, Logger>
+  builder: Builder
   handlers: Handler[]
 
-  constructor(name: string, defaultLevel: number = DEFAULT_LEVELS.INFO, handlers?: Handler[]) {
-    this.name = name
+  constructor(name: string, defaultLevel: number = DEFAULT_LEVELS.INFO, builder: Builder = defaultBuilder, handlers?: Handler[]) {
+    this._name = name
     this.defaultLevel = defaultLevel
+    this.builder = builder
     this.handlers = handlers ?? []
     this.propagate = true
     this.children = {}
   }
 
-  trace(log: Log): void {
-    this.log(log, DEFAULT_LEVELS.TRACE)
-  }
-
-  debug(log: Log): void {
-    this.log(log, DEFAULT_LEVELS.DEBUG)
-  }
-
-  info(log: Log): void {
-    this.log(log, DEFAULT_LEVELS.INFO)
-  }
-
-  warn(log: Log): void {
-    this.log(log, DEFAULT_LEVELS.WARN)
-  }
-
-  error(log: Log): void {
-    this.log(log, DEFAULT_LEVELS.ERROR)
-  }
-
-  fatal(log: Log): void {
-    this.log(log, DEFAULT_LEVELS.FATAL)
+  get name(): string {
+    return this._name
   }
 
   addHandler(handler: Handler) {
@@ -96,10 +113,36 @@ class Logger implements Loggable {
     return child
   }
 
-  log(log: Log, level?: number): void {
-    this.handlers.forEach(handler => handler.level <= (level ?? this.defaultLevel) && handler.log(log))
-    if (this.propagate) this.parent?.log(log)
+  log(log: LogRecord, level?: number): void {
+    const _log = {...log, level: Logging.getLevel(level ?? this.defaultLevel)}
+    this.handlers.forEach(handler => handler.level <= (level ?? this.defaultLevel) && handler.handle(_log))
+    if (this.propagate) this.parent?.log(_log, level)
   }
+
+  trace(...args: unknown[]): void {
+    this.log(this.builder.build(...args), DEFAULT_LEVELS.TRACE)
+  }
+
+  debug(...args: unknown[]): void {
+    this.log(this.builder.build(...args), DEFAULT_LEVELS.DEBUG)
+  }
+
+  info(...args: unknown[]): void {
+    this.log(this.builder.build(...args), DEFAULT_LEVELS.INFO)
+  }
+
+  warn(...args: unknown[]): void {
+    this.log(this.builder.build(...args), DEFAULT_LEVELS.WARN)
+  }
+
+  error(...args: unknown[]): void {
+    this.log(this.builder.build(...args), DEFAULT_LEVELS.ERROR)
+  }
+
+  fatal(...args: unknown[]): void {
+    this.log(this.builder.build(...args), DEFAULT_LEVELS.FATAL)
+  }
+
 }
 
 class RootLogger extends Logger {
@@ -122,35 +165,58 @@ class RootLogger extends Logger {
     }
     return this.children[name]
   }
+
+  log(log: LogRecord, level?: number) {
+    super.log({...log, name: "root"}, level);
+  }
 }
 
 class Logging {
   private static _root: Logger = RootLogger.instance
+  private static _levels: Levels = {...DEFAULT_LEVELS}
+
   static get root() {
     return Logging._root
+  }
+
+  static get levels() {
+    return {...Logging._levels}
   }
 
   static addLevel(name: string, level: number) {
     Object.defineProperties(Logging, {
       [name.toUpperCase()]: {
         configurable: false,
-        value: level
+        get() {
+          return Logging.levels[name.toUpperCase() as keyof Levels]
+        }
       },
       [name.toLowerCase()]: {
         configurable: false,
-        value: (log: Log) => Logging.log(log, level)
+        value: (...args: unknown[]) => Logging.root[name.toLowerCase() as keyof Loggable](...args)
       },
     })
-    ;(Logger.prototype as any)[name.toLowerCase()] = function (this: Logger, log: Log) {
-      return this.log(log, level)
+    ;(Logger.prototype as any)[name.toLowerCase()] = function (this: Logger, ...args: unknown[]) {
+      return this.log(this.builder.build(...args), level)
     }
   }
 
   static getLogger(name: string) {
+    if (name === "root") return Logging.root
     return name.split(LOGGER_SEPARATOR).reduce((logger, name) => logger.getChild(name), Logging.root)
   }
 
-  static log(log: Log, level?: number) {
+  static getLevel(value: number): string
+  static getLevel(value: keyof Loggable): number
+  static getLevel(value: keyof Loggable | number) {
+    if (typeof value === "string") return (Logging as any)[value]
+    return Object.keys(Logging.levels).reduce(
+      (_value, key) => String(Logging.levels[key as keyof Levels]) === _value ? key : _value,
+      String(value)
+    )
+  }
+
+  static log(log: LogRecord, level?: number) {
     Logging.root.log(log, level)
   }
 }
